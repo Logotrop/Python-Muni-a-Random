55c59b8c33ca0f6003491aabcc2ba6b25ddef7de
Logotrop <keramdobes@gmail.com>
2019-09-25 16:36:26 +0200

    version 1.0

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 import sentry_sdk
 sentry_sdk.init("https://f579bdfd186e4263a310d4e6a8c0543e@sentry.io/1759097")
-f = 0/0
+limitn = int(input("Zadej minimum:"))
+limitm = int(input("Zadej maximum:"))
+
+def test(n, m):
+    result = "Žádný výsledek"
+    for a in range(n, m):
+        for b in range(n, m):
+            for c in range(n, m):
+                if pow(a, 2) + pow(b, 2) == 3 * pow(c, 2):
+                    result = ("Hodnoty jsou:",a,",",b,",",c)
+                    break
+                #else:
+                    #print(a,",",b,",",c)
+    print("Test ukončen pro interval ",n," - ",m)
+    return result
+    
+m = limitn + 100
+n = limitn
+while limitm>m:
+    m += 100
+    test(n,m)
