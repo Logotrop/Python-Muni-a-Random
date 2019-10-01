93f791899b81fa1fa8e5fb3917eae7eb2e24bf92
Logotrop <keramdobes@gmail.com>
2019-10-01 12:22:43 +0200

    oprava turtle

--- turtle-test.py
+++ turtle-test.py
@@ -1,54 +1,22 @@
-<<<<<<< HEAD
-=======
 #import sentry_sdk
 #sentry_sdk.init("https://e1bb0649f12642f4ad177f535719a6ee@sentry.io/1761313")
->>>>>>> d9423b6c4f2199e37b5539b5c2751dcab588f503
 import turtle
 zelva = turtle.Turtle()
 zelva.color('black')
 zelva.pensize(5)
 zelva.shape('turtle')
 
-<<<<<<< HEAD
-telo = 150
-sirka = 130
-uhelsipky = 90 # 60 pro rovnou šipku
-velikostsipky = 200
-pravyuhel = 90 #duh
-
-n = (velikostsipky - sirka)/2 #konce sipky 
-m = 180 - uhelsipky #vypocet uhlu
-
-zelva.forward(telo)
-zelva.right(pravyuhel)
-zelva.forward(n)
-zelva.left(m)
-zelva.forward(velikostsipky)
-zelva.left(m)
-zelva.forward(velikostsipky)
-zelva.right(uhelsipky)
-zelva.backward(n)
-zelva.left(pravyuhel)
-zelva.forward(telo)
-zelva.left(pravyuhel)
-zelva.home()
-#zelva.forward(sirka)
+zelva.forward(50)
+zelva.right(90)
+zelva.forward(40)
+zelva.left(135)
+zelva.forward(100)
+zelva.left(90)
+zelva.forward(100)
+zelva.right(45)
+zelva.backward(40)
+zelva.left(90)
+zelva.forward(50)
+zelva.left(90)
+zelva.forward(61.421356237)
 zelva.hideturtle()
-input()
-exit
-=======
-tim.forward(50)
-tim.right(90)
-tim.forward(40)
-tim.left(135)
-tim.forward(100)
-tim.left(90)
-tim.forward(100)
-tim.right(45)
-tim.backward(40)
-tim.left(90)
-tim.forward(50)
-tim.left(90)
-tim.forward(61.421356237)
-tim.hideturtle()
->>>>>>> d9423b6c4f2199e37b5539b5c2751dcab588f503
